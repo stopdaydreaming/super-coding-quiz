8d2fb22a4fc61c8e1aaef30937c71479ab126908
stopdaydreaming <hello@stopdaydreaming.com>
2020-12-14 21:10:47 -0500

    logging correct values

--- assets/scripts.js
+++ assets/scripts.js
@@ -25,17 +25,17 @@
   // user input initials
   // user can go back or clear scores
 
-var startContentEl = document.querySelector("#start-content");
+var startContentEl = document.querySelector('#start-content');
 
-var timerEl = document.querySelector("#timer");
-var startQuizEl = document.querySelector("#start-quiz");
+var timerEl = document.querySelector('#timer');
+var startQuizEl = document.querySelector('#start-quiz');
 
-var questionEl = document.querySelector("#question");
-var answersEl = document.querySelector("#answer");
-var quizContentEl = document.querySelector("#quiz-content");
-var answerFeedbackEl = document.querySelector("#answer-feedback");
+var questionEl = document.querySelector('#question');
+var answersEl = document.querySelector('#answer');
+var quizContentEl = document.querySelector('#quiz-content');
+var answerFeedbackEl = document.querySelector('#answer-feedback');
 
-var testBtnEl = document.querySelector("#test-btn");
+var testBtnEl = document.querySelector('#test-btn');
 
 var totalSeconds = 0;
 var secondsElapsed = 0;
@@ -47,22 +47,22 @@ timerEl.textContent = 0;
 
 var quiz = [
   {
-    question: "Commonly used data types DO NOT include:",
+    question: 'Commonly used data types DO NOT include:',
     answers: ['1. strings', '2. booleans', '3. alerts', '4. numbers'],
-    corrAnswer: 2
+    corrAnswer: '2. booleans'
   },
   {
-    question: "The condition in an if/else statement is enclosed within ______",
+    question: 'The condition in an if/else statement is enclosed within ______',
     answers: ['1. quotes', '2. curly braces', '3. parenthesis', '4. square brackets'],
     corrAnswer: 2
   },
   {
-    question: "Arrays in JavaScript can be used to store",
+    question: 'Arrays in JavaScript can be used to store',
     answers: ['1. numbers and strings', '2. other arrays', '3. booleans', '4. all of the above'],
     corrAnswer: 3
   },
   {
-    question: "String values must be enclosed within _______ when being assigned to variables.",
+    question: 'String values must be enclosed within _______ when being assigned to variables.',
     answers: ['1. commas', '2. curly brackets', '3. quotes', '4. parenthesis'],
     corrAnswer: 1
   }
@@ -79,10 +79,8 @@ function showQuestion(currQuestionIndex){
       a.textContent = quiz[currQuestionIndex].answers[i];
       answersEl.appendChild(a);
       a.addEventListener('click', startTimer);
-    
-
       // create break between buttons
-      var br = document.createElement("br");
+      var br = document.createElement('br');
       answersEl.appendChild(br);
       // event listener for button, which button did i click
       // if statement, currQuestionIndex++
@@ -103,7 +101,25 @@ function endQuiz(){
 function nextQuestion(){}
 
 function selectAnswer(){
+  // console.log('select answer');
+  for(var i = 0; i < quiz[currQuestionIndex].answers.length; i++ ) {
+      // LOGS CORRECT VALUES
+      var x = quiz[currQuestionIndex].answers[i];
+      var y = quiz[currQuestionIndex].corrAnswer;
+      console.log('all answers: ' + x);
+      console.log('correct answer: ' + y);
+
+    // var answer = quiz[currQuestionIndex].answers[i];
+    // var correctAnswer = quiz[currQuestionIndex].corrAnswer;
+    // if( answer === correctAnswer){
+    //     console.log("correct answer");
+    // }
+    // else {
+    //   console.log("incorrect");
+    // }
+  }
 // figure out if answer was correct and keep track of it
+
 // increment the variable that is keeping track of which question we're on
 // call showQuestion and pass in the new variable value we changed in the previous step
 // figure out when the quiz is over and handle it when it ends
@@ -125,6 +141,7 @@ function startTimer() {
       timerEl.textContent = getSeconds(totalSeconds);
     }
   }, 1000);
+  answersEl.addEventListener("click", selectAnswer)
 }
 
 function getSeconds(time) {
@@ -132,7 +149,7 @@ function getSeconds(time) {
   return seconds;
 }
 
-startQuizEl.addEventListener("click", startQuiz);
-// NEED TO FIX: highscores page has this error "scripts.js:86 Uncaught TypeError: Cannot read property 'addEventListener' of null" because start quiz doesnt exist here
+startQuizEl.addEventListener('click', startQuiz);
+// NEED TO FIX: highscores page has this error 'scripts.js:86 Uncaught TypeError: Cannot read property 'addEventListener' of null' because start quiz doesnt exist here
 
-testBtnEl.addEventListener("click", endQuiz);
+// testBtnEl.addEventListener('click', endQuiz);
